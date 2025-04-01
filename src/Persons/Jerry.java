293fe0886b6b0e53ef1d6a9f0864af92f2e31862
MrJerry <my_main.com>
2025-04-01 21:28:39 +0300

    1 added class Jerry

---
+++ src/Persons/Jerry.java
@@ -0,0 +1,13 @@
+package Persons;
+
+import Core.Poem;
+
+public class Jerry extends Person {
+
+    @Override
+    public void addTextToPoem() {
+//        Poem.getInstance().addLineAtIndex(20, ""); //Росою-сльозою...
+//        Poem.getInstance().addLineAtIndex(22, ""); //«Брате мій, з тобою
+//        Poem.getInstance().addLineAtIndex(24, ""); //А я й не сказала,
+    }
+}
