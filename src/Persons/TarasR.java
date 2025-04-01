9b77e51ed93619ce7257da062a10fa0c2f268e09
Taras <rtvlutsk2006@gmail.com>
2025-04-01 19:09:46 +0200

    TarasR

---
+++ src/Persons/TarasR.java
@@ -0,0 +1,13 @@
+package Persons;
+
+import Core.Poem;
+
+public class TarasR extends Person {
+
+    @Override
+    public void addTextToPoem(){
+
+        Poem.getInstance().addLineAtIndex(1," ");
+        Poem.getInstance().addLineAtIndex(1," ");
+    }
+}
