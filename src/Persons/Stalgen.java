1e3119277aea31250946c84ce8920f3cc47ce2e9
stalgen <stalgen51@gmail.com>
2025-04-01 17:36:29 +0300

    added Stalgen

---
+++ src/Persons/Stalgen.java
@@ -0,0 +1,10 @@
+package Persons;
+
+public class Stalgen extends Person{
+    public void addTextToPoem(){
+
+//        Poem.getInstance().addLineAtIndex(1,"");
+//        Poem.getInstance().addLineAtIndex(1,"");
+
+    }
+}
