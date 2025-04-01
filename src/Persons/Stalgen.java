c6e97764c5dc91c070b2e98501935ee5ec2cf222
stalgen <stalgen51@gmail.com>
2025-04-02 00:39:16 +0300

    edit Stalgen

--- src/Persons/Stalgen.java
+++ src/Persons/Stalgen.java
@@ -1,10 +1,12 @@
 package Persons;
 
+import Core.Poem;
+
 public class Stalgen extends Person{
-    public void addTextToPoem(){
 
-//        Poem.getInstance().addLineAtIndex(0,"");
-//        Poem.getInstance().addLineAtIndex(0,"");
-
+    @Override
+    public void addTextToPoem() {
+//        Poem.getInstance().addLineAtIndex(1,  " ");
+//        Poem.getInstance().addLineAtIndex(5,  " ");
     }
 }
