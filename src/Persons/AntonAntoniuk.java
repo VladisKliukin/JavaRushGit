9efa66436581d8503b376590b2bbb734e8087a3f
AntonAntoniuk <anton.antonyuck@gmail.com>
2025-04-01 21:25:29 +0300

    add class AntonAntoniuk

---
+++ src/Persons/AntonAntoniuk.java
@@ -0,0 +1,11 @@
+package Persons;
+
+public class AntonAntoniuk extends Person {
+    public void addTextToPoem() {
+        /*
+        Poem.getInstance().addLineAtIndex(6,  " ");
+        Poem.getInstance().addLineAtIndex(16,  " ");
+        Poem.getInstance().addLineAtIndex(25,  " ");
+        */
+    }
+}
