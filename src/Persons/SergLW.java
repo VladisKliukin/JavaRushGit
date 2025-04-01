197c6393cb52d2fbd23602e3f07b557188f85a01
serglw <lionwind88@gmail.com>
2025-04-01 22:12:40 +0200

    Add class SergLW + to participants.txt Persons.SergLW

---
+++ src/Persons/SergLW.java
@@ -0,0 +1,12 @@
+package Persons;
+
+import Core.Poem;
+
+public class SergLW extends Person {
+    @Override
+    public void addTextToPoem() {
+        //Poem.getInstance().addLineAtIndex();
+        //Poem.getInstance().addLineAtIndex();
+        //Poem.getInstance().addLineAtIndex();
+    }
+}
