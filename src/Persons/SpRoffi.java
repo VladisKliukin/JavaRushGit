a3ba769fa27bcac83b4ac81a9d03622db8421912
SpRoffi <sersemsed@gmail.com>
2025-04-02 21:57:18 +0300

    Added text

--- src/Persons/SpRoffi.java
+++ src/Persons/SpRoffi.java
@@ -1,15 +1,17 @@
 package Persons;
 
-//import Core.Poem;
+import Core.Poem;
 
 public class SpRoffi extends Person{
 
     @Override
     public void addTextToPoem(){
 
-//        Poem.getInstance().addLineAtIndex(1,"");
-//        Poem.getInstance().addLineAtIndex(1,"");
-//        Poem.getInstance().addLineAtIndex(1,"");
+        Poem.getInstance().addLineAtIndex(7,"Царівною називають,");
+        Poem.getInstance().addLineAtIndex(33,"Хто їй лихо заподіяв?");
+        Poem.getInstance().addLineAtIndex(35,"Я гралася, забавлялась,");
+        Poem.getInstance().addLineAtIndex(59,"Молодого, короткого");
+        Poem.getInstance().addLineAtIndex(88,"Червоно-рожеву");
 
     }
 }
