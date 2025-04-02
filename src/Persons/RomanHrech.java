251c7a10feadc2f152ec7d4474a270b07495245f
Roman <grechoroma@gmail.com>
2025-04-02 21:33:42 +0300

    added final lines

--- src/Persons/RomanHrech.java
+++ src/Persons/RomanHrech.java
@@ -6,6 +6,10 @@ public class RomanHrech extends Person {
     @Override
     public void addTextToPoem() {
 
-        Poem.getInstance().addLineAtIndex(12, "Королевий Цвіте!»");
+        Poem.getInstance().addLineAtIndex(12, "Королевий Цвіте!");
+        Poem.getInstance().addLineAtIndex(69, "Люди добрі! Чи правдиво");
+        Poem.getInstance().addLineAtIndex(53, "А весною я процвіла");
+        Poem.getInstance().addLineAtIndex(65, "Царівною називають,");
+        Poem.getInstance().addLineAtIndex(70, "Те, що говорила");
     }
 }
