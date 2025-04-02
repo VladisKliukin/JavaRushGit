4123ebfd548002e33e8d6a24da365d02dfccfee7
Andriy Tsomko <andriytsomkoo@gmail.com>
2025-04-02 21:03:28 +0300

    add second line

--- src/Persons/AndriyTsomko.java
+++ src/Persons/AndriyTsomko.java
@@ -7,5 +7,6 @@ public class AndriyTsomko extends Person{
     @Override
     public void addTextToPoem() {
         Poem.getInstance().addLineAtIndex(2, "Люде не любили?");
+        Poem.getInstance().addLineAtIndex(8, "Очей не спускають");
     }
 }
