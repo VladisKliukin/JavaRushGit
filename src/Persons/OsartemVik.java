4f509f53c75da0260b7779ec9f07a55140388769
”Artem <26and06@gmail.com>
2025-04-02 22:02:18 +0300

    Added Lilea.txt and modifed OsartemVik.java

--- src/Persons/OsartemVik.java
+++ src/Persons/OsartemVik.java
@@ -2,11 +2,24 @@ package Persons;
 
 import Core.Poem;
 
-public class OsartemVik extends Person{
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class OsartemVik extends Person {
     @Override
     public void addTextToPoem() {
+        Path path = Path.of("src\\Lilea.txt");
+        try {
+            List<String> list = Files.readAllLines(path);
+            for (int i = 0; i < list.size(); i++) {
+                if (i == 36 || i == 39 || i == 47 || i == 71 || i == 76)
+                    Poem.getInstance().addLineAtIndex((i + 1), list.get(i));
+            }
 
-//        Poem.getInstance().addLineAtIndex(1,  " ");
-//        Poem.getInstance().addLineAtIndex(5,  " ");
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
     }
 }
