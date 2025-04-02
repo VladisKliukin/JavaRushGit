200e052e9e8622d8b018e5237d2eebfbd3a4114b
Nick <bigmike.ua@gmail.com>
2025-04-02 11:30:33 +0300

    add Nick second commit

--- src/Persons/NickF.java
+++ src/Persons/NickF.java
@@ -1,4 +1,11 @@
 package Persons;
 
-public class NickF {
+import Core.Poem;
+
+public class NickF extends Person {
+    @Override
+    public void addTextToPoem() {
+        Poem.getInstance().addLineAtIndex(4, " ");
+        Poem.getInstance().addLineAtIndex(9, " ");
+    }
 }
