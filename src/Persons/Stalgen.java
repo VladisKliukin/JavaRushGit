e502dc9b9eefe6776a8ffe9526b30edffba49d5f
stalgen <stalgen51@gmail.com>
2025-04-02 21:29:26 +0300

    Stalgen 38

--- src/Persons/Stalgen.java
+++ src/Persons/Stalgen.java
@@ -6,7 +6,10 @@ public class Stalgen extends Person{
 
     @Override
     public void addTextToPoem() {
-//        Poem.getInstance().addLineAtIndex(1,  " ");
-//        Poem.getInstance().addLineAtIndex(5,  " ");
+        Poem.getInstance().addLineAtIndex(38,  "Кляла-проклинала.");
+//        Poem.getInstance().addLineAtIndex(25,  "Як була я людиною,");
+//        Poem.getInstance().addLineAtIndex(34,  "Я була дитина,");
+//        Poem.getInstance().addLineAtIndex(13,  "«Я не знаю, моя сестро».");
+//        Poem.getInstance().addLineAtIndex(68,  "В хату не пустили.");
     }
 }
