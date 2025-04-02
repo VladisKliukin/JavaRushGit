0c616c25f2f1d2466a82ce315ba658870fd39a23
Andriy Tsomko <andriytsomkoo@gmail.com>
2025-04-02 22:27:30 +0300

    correct

--- src/Persons/AndriyTsomko.java
+++ src/Persons/AndriyTsomko.java
@@ -8,8 +8,8 @@ public class AndriyTsomko extends Person{
     public void addTextToPoem() {
         Poem.getInstance().addLineAtIndex(2, "Люде не любили?");
         Poem.getInstance().addLineAtIndex(8, "Очей не спускають");
-        Poem.getInstance().addLineAtIndex(63, "Зимою люде... Боже мій!");
-        Poem.getInstance().addLineAtIndex(67, "А весною, мов на диво,");
-        Poem.getInstance().addLineAtIndex(75, "А дівчата заквітчались");
+        Poem.getInstance().addLineAtIndex(63, "А весною процвіла я");
+        Poem.getInstance().addLineAtIndex(67, "Зимою люде... Боже мій!");
+        Poem.getInstance().addLineAtIndex(75, "Стала в гаї, і в теплиці,");
     }
 }
