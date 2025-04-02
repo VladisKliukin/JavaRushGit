34ffaf5ca94ad1a251c46a1f1696212a1b1d71bd
MrJerry <my_main.com>
2025-04-02 20:38:31 +0300

    added_2st_line

--- src/Persons/Jerry.java
+++ src/Persons/Jerry.java
@@ -7,7 +7,7 @@ public class Jerry extends Person {
     @Override
     public void addTextToPoem() {
         Poem.getInstance().addLineAtIndex(82, "Тих самих, що вбили");
-//        Poem.getInstance().addLineAtIndex(22, "");
+        Poem.getInstance().addLineAtIndex(10, "Не знають, де діти!");
 //        Poem.getInstance().addLineAtIndex(24, "");
     }
 }
