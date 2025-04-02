7a9c8a2505cc88d8074dc294726b139a7ec9ebdb
”Lamaret” <san4eknefedov@gmail.com>
2025-04-02 21:46:35 +0300

    add Lamaret 32

--- src/Persons/Lamaret.java
+++ src/Persons/Lamaret.java
@@ -7,7 +7,7 @@ public class Lamaret extends Person{
     public void addTextToPoem() {
 
         Poem.getInstance().addLineAtIndex(18,  "Личенька Лілеї.");
-//        Poem.getInstance().addLineAtIndex(32,  "Мій брате єдиний!");
+        Poem.getInstance().addLineAtIndex(32,  "Мій брате єдиний!");
 //        Poem.getInstance().addLineAtIndex(52,  "Остригли, накрили");
 //        Poem.getInstance().addLineAtIndex(54,  "Та ще й реготались.");
 //        Poem.getInstance().addLineAtIndex(84,  "Святий Боже милий!»");
