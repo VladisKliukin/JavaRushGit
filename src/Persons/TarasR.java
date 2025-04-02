aca8297c80a9772c06a66c51a76eaf1626e77d17
Taras <rtvlutsk2006@gmail.com>
2025-04-02 21:28:31 +0200

    //Commit - Taras - lines(50,80,78,31,22)

--- src/Persons/TarasR.java
+++ src/Persons/TarasR.java
@@ -6,9 +6,12 @@ public class TarasR extends Person {
 
     @Override
     public void addTextToPoem(){
-//Commit
-        Poem.getInstance().addLineAtIndex(1," ");
-        Poem.getInstance().addLineAtIndex(1," ");
+//Commit - Taras - lines(50,80,78,31,22)
+        Poem.getInstance().addLineAtIndex(50,  "Убити не вбили,");
+        Poem.getInstance().addLineAtIndex(80,  "Цвітом на сім світі?");
+        Poem.getInstance().addLineAtIndex(78,  "Королевий Цвіте,");
+        Poem.getInstance().addLineAtIndex(31,  "І плакала? Я не знаю,");
+        Poem.getInstance().addLineAtIndex(22,  "«Брате мій, з тобою");
 
     }
 }
