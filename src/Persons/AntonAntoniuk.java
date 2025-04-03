7d69f5b619ca114808ad20fe542bf5f43fa5d33c
AntonAntoniuk <anton.antonyuck@gmail.com>
2025-04-03 22:49:28 +0300

    - Fix poem_user_data.csv file location. - Add PoemLine.java class. - Change AntonAntoniuk.java class (change override method  addTextToPoem.

--- src/Persons/AntonAntoniuk.java
+++ src/Persons/AntonAntoniuk.java
@@ -1,13 +1,30 @@
 package Persons;
 
 import Core.Poem;
+import Core.PoemLine;
+
+import java.util.List;
+
+import static Core.PoemLine.getPoemLinesByUser;
 
 public class AntonAntoniuk extends Person {
+    String filePath = "src\\poem_user_data.csv";
+    String separSign = ";";
+    String userName = "AntonAntoniuk";
 
     @Override
     public void addTextToPoem() {
 
-        Poem.getInstance().addLineAtIndex(6, "В палатах вітають,");
+        List<PoemLine> poemLines = getPoemLinesByUser(filePath, userName, separSign);
+
+        if (poemLines.isEmpty()) {
+            System.out.println("Не знайдено рядків для користувача: " + userName);
+        } else {
+
+            for (PoemLine poemLine : poemLines) {
+                Poem.getInstance().addLineAtIndex(poemLine.getLineNumber(), poemLine.getLineText());
+            }
+        }
 
     }
 }
