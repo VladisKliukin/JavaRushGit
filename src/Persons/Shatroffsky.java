6b207510beeff7410572032ff302977d6f5af4af
Vladyslav Shatrovskyi <shatroffsky@gmail.com>
2025-04-04 14:27:28 +0300

    added 5 strings for poem.

--- src/Persons/Shatroffsky.java
+++ src/Persons/Shatroffsky.java
@@ -5,6 +5,10 @@ public class Shatroffsky extends Person{
 
     @Override
     public void addTextToPoem() {
-        Poem.getInstance().addLineAtIndex(3, " ");
+        Poem.getInstance().addLineAtIndex(3, "За що мене, як виросла");
+        Poem.getInstance().addLineAtIndex(14, "І Цвіт Королевий");
+        Poem.getInstance().addLineAtIndex(41, "Я виросла, викохалась");
+        Poem.getInstance().addLineAtIndex(73, "Лілеєю-снігоцвітом;");
+        Poem.getInstance().addLineAtIndex(79, "Нащо мене Бог поставив");
     }
 }
